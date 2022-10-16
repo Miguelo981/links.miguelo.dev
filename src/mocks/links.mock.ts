abad31765a986519ea9cc401c7979f7a010ed25a
Miguelo981 <lorito20@hotmail.es>
2022-10-16 13:35:44 +0200

    - Add Miguelo Links static code

---
+++ src/mocks/links.mock.ts
@@ -0,0 +1,26 @@
+import type { LinkGroup } from "../types";
+
+export const groups: LinkGroup[] = [
+    {
+        title: "Portfolio",
+        links: [
+            {
+                title: "Miguelo DEV",
+                link: "https://miguelo.dev/es",
+            },
+        ]
+    },
+    {
+        title: "Proyectos",
+        links: [
+            {
+                title: "Info Takushi JP 🚕",
+                link: "http://takushi.jp.net/",
+            },
+            {
+                title: "Web3 Dashboard",
+                link: "https://web3.miguelo.dev",
+            }
+        ]
+    }
+]

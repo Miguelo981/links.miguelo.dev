575caf4a32c89715885c623a2715f96ee2c140e2
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2025-02-02 16:24:53 +0000

    Add deployment

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:lts AS runtime
+WORKDIR /app
+
+COPY . .
+
+RUN npm install -g pnpm
+
+RUN pnpm i
+RUN pnpm build
+
+ENV HOST=0.0.0.0
+ENV PORT=8080
+EXPOSE 8080
+
+CMD node ./dist/server/entry.mjs
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
